a6247c587a70071534aaa33a624a3ff9a33cabbd
PradHolla <pradholla@gmail.com>
2023-03-14 17:05:28 +0530

    Added OPTIONS for preflight request handling

--- app/app.py
+++ app/app.py
@@ -101,7 +101,6 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "headers": {
-            "Content-Type": "application/json",
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Headers": "*",
             "Access-Control-Allow-Methods": "*"
