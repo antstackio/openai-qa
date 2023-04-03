834a71995d680887ff96575b86e6b2162ae14cdc
PradHolla <pradholla@gmail.com>
2023-04-04 00:09:49 +0530

    Added Pinecone

--- app/app.py
+++ app/app.py
@@ -1,71 +1,57 @@
-import os
 import boto3
 import json
-import base64
 import openai
-from openai.embeddings_utils import distances_from_embeddings, cosine_similarity
-import pandas as pd
-import numpy as np
-# from dotenv import load_dotenv
-# load_dotenv()
+import pinecone
 
 ssm = boto3.client('ssm')
-# OPENAI_API_KEY = ssm.get_parameter(Name='OPENAI_API_KEY', WithDecryption=True)['Parameter']['Value']
 openai.api_key = ssm.get_parameter(Name='OPENAI_API_KEY', WithDecryption=True)['Parameter']['Value']
+pinecone_key = ssm.get_parameter(Name='PINECONE_KEY', WithDecryption=True)['Parameter']['Value']
+pinecone_env = ssm.get_parameter(Name='PINECONE_ENV', WithDecryption=True)['Parameter']['Value']
 
-df = pd.read_pickle('/opt/ml/embeddings.pkl')
+index = pinecone.Index('openai')
 
 def create_context(
-    question, df, max_len=1800, size="ada"
+    question,
 ):
     """
-    Create a context for a question by finding the most similar context from the dataframe
+    Create a context for a question by finding the most similar context from the pinecone index
     """
-
+    max_len=1800
     # Get the embeddings for the question
     q_embeddings = openai.Embedding.create(input=question, engine='text-embedding-ada-002')['data'][0]['embedding']
-
+    
     # Get the distances from the embeddings
-    df['distances'] = distances_from_embeddings(q_embeddings, df['embeddings'].values, distance_metric='cosine')
-
+    res = index.query([q_embeddings], top_k=5, include_metadata=True)
 
     returns = []
     cur_len = 0
 
     # Sort by distance and add the text to the context until the context is too long
-    for i, row in df.sort_values('distances', ascending=True).iterrows():
-        
+    for match in res['matches']:
+        text = match['metadata']['text']
+        n_tokens = match['metadata']['n_tokens']
         # Add the length of the text to the current length
-        cur_len += row['n_tokens'] + 4
-        
+        cur_len += n_tokens + 4
         # If the context is too long, break
         if cur_len > max_len:
             break
-        
         # Else add it to the text that is being returned
-        returns.append(row["text"])
-
+        returns.append(text)
     # Return the context
     return "\n\n###\n\n".join(returns)
 
 def answer_question(
-    df,
     model="gpt-3.5-turbo",
     question="What is AntStack?",
-    max_len=1800,
-    size="ada",
     debug=False,
     max_tokens=150,
     stop_sequence=None
 ):
     """
-    Answer a question based on the most similar context from the dataframe texts
+    Answer a question based on the most similar context from pinecone
     """
     context = create_context(
         question,
-        df,
-        max_len=max_len,
-        size=size,
     )
     # If debug, print the raw model response
     if debug:
@@ -94,7 +80,7 @@ def answer_question(
 def lambda_handler(event, context):
     body = json.loads(event["body"])
     prompt = body["prompt"]
-    response = answer_question(df, question=prompt, debug=False)
+    response = answer_question(question=prompt)
     
     return {
         "statusCode": 200,
